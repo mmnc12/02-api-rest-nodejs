c53cdfc50ebd6c2888d7faa4293747b0d21ed64d
Manoel Mecias Nascimento <you@example.com>
2023-10-12 01:10:03 -0300

    Prepare correct

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,15 @@
-import { knex as setupKnex, Knex } from 'knex';
+import 'dotenv/config';
+import { Knex,  knex as setupKnex } from 'knex';
 import { env } from './env';
 
-export const config = {
+export const config: Knex.Config = {
     client: env.DATABASE_CLIENT,
-    connection: env.DATABASE_CLIENT === 'sqlite' ? {
-        filename: env.DATABASE_URL,
-    } : env.DATABASE_CLIENT,
+    connection: 
+        env.DATABASE_CLIENT === 'sqlite'
+         ? {
+            filename: env.DATABASE_URL,
+        } 
+        : env.DATABASE_CLIENT,
     useNullAsDefault: true,
     migrations: {
         extension: 'ts',
