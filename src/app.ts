63bf06fc22848af627ad3981112ce2a740e9f894
Manoel Mecias Nascimento <you@example.com>
2023-10-12 07:32:32 -0300

    revisando js

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import fastify from "fastify";
 import cookie from "@fastify/cookie";
-
-import { env } from "./env";
 import { transactionsRoutes } from "./routes/transactions";
 
 export const app = fastify()
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { app } from './app';
 import { env } from './env';
 
+const port = env.PORT;
+const host = "RENDER" in process.env ? "0.0.0.0" : "localhost";
 
-app.listen({
-    port: env.PORT,
+app.listen({ 
+    host, port
 }).then(() => {
     console.log('HTTP Server Running!')
 })
